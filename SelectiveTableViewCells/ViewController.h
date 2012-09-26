73117f9de3d7cc3c41d080a40b1a478a8e014f38
Nandeep mali <nandeep@miniorb.in>
2012-09-26 13:57:04 +0530

    Initial Commit

---
+++ SelectiveTableViewCells/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SelectiveTableViewCells
+//
+//  Created by Nandeep Mali on 26/09/12.
+//  Copyright (c) 2012 Nandeep Mali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SelectiveTableViewCells/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SelectiveTableViewCells
+//
+//  Created by Nandeep Mali on 26/09/12.
+//  Copyright (c) 2012 Nandeep Mali. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
